c1bf4b6cd26ba51893c5ca7f4123c774c1e89989
sjp <794467118@qq.com>
2023-03-01 09:57:27 +0800

    更新'

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
-    description: "Site description",
-    site: 'https://astro-blog.qum.cc',
+    title: "孙亖哥的博客",
+    description: "孙亖哥的博客，谢谢技术笔记、生活随笔等等。",
+    site: 'https://blog.side4u.cn',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>zh-cn</language>`,
   });
